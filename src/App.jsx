88749a0d0b3b4f906f915f8f68ac496944d0c4f2
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-06 21:55:40 +0000

    updated the directory for logo and made changes to App.jsx appropriately

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import appLogo from '/assets/logo.png'
+import appLogo from '/logo.png'
 import './App.css'
 
 function App() {
