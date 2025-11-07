f4f4ab1f47bc72888971863801bfe8780b53f979
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-07 14:34:38 +0000

    trying Navbar component for first time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import appLogo from '/logo.png'
+import Nav from './components/Nav'
 import './App.css'
 
 export default function App() {
@@ -6,30 +7,7 @@ export default function App() {
 
   return (
     <>
-      <div class="container-fluid p-5 bg-primary text-white text-center">
-        <h1>My First Bootstrap Page</h1>
-        <p>Resize this responsive page to see the effect!</p> 
-      </div>
-        
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-sm-4">
-            <h3>Column 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-          </div>
-          <div class="col-sm-4">
-            <h3>Column 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-          </div>
-          <div class="col-sm-4">
-            <h3>Column 3</h3>        
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-          </div>
-        </div>
-      </div>
+      {Nav}
     </>
   )
 }
