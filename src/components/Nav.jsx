26a0172ef95857662eac471a1a07305cb9ce6c18
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-07 15:52:12 +0000

    tewaked logo with new itteration

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
       <header>
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand d-flex align-items-center" href=""><img src={appLogo} alt="Jolen's Property Checker Logo"/>&nbsp;<span className="julius-sans-one-regular">PROP80</span></a>
+                <a className="navbar-brand" href=""><img src={appLogo} alt="Jolen's Property Checker Logo"/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span className="navbar-toggler-icon"></span>
                 </button>
