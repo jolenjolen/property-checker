096fa89b3a5a2ecf6deb6f4f8260c418a14859c6
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-07 15:44:08 +0000

    changed some font aspects

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
       <header>
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand d-flex align-items-center" href=""><img src={appLogo} alt="Jolen's Property Checker Logo"/>&nbsp;<h3 className="julius-sans-one-regular m-0">PROP80</h3></a>
+                <a className="navbar-brand d-flex align-items-center" href=""><img src={appLogo} alt="Jolen's Property Checker Logo"/>&nbsp;<span className="julius-sans-one-regular">PROP80</span></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span className="navbar-toggler-icon"></span>
                 </button>
