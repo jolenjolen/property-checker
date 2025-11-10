bb3d510de8fff0d253a5b188776b193578349ed8
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-10 18:55:56 +0000

    favourites btn updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -34,7 +34,7 @@ export default function Nav() {
           </ul>
           <button className={`favouriteBtn mx-1 p-1 border-2 d-flex justify-content-center align-items-center rounded-pill btn btn-outline-danger`} title="View Your Favourites">
             <span>&nbsp;&nbsp;My&nbsp;Favourites</span>
-            &nbsp;
+            &nbsp;&nbsp;
             <span className="m-0 material-symbols-rounded bg-danger text-white rounded-pill d-flex justify-content-center align-items-center">
               favorite
             </span>
