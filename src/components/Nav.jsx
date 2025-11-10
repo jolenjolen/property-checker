d78b3eaed241a69560e64a4b34a03873d6f904da
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-10 19:31:29 +0000

    favourites btn updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -48,7 +48,7 @@ export default function Nav() {
       </nav>
       <div className={`offcanvas offcanvas-end bg-${theme} text-${inverseTheme}`} id="favourites">
         <div className="offcanvas-header d-flex justify-content-between align-items-center">
-          <h4 className="offcanvas-title">Heading</h4>
+          <h4 className="offcanvas-title">My Favourites</h4>
           <button data-bs-dismiss="offcanvas" className={`mx-1 closeFavourites d-flex justify-content-center align-items-center rounded-pill btn btn-${inverseTheme}`} title="Close Favourites"><span className="material-symbols-rounded">close</span></button>
         </div>
         <div className="offcanvas-body">
