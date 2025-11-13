66074039085dc39e4e744f844499c1b2a51f08a9
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-13 14:31:44 +0000

    removed router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,8 @@
 import appLogo from "/logo.png";
 import "../App.css";
 import { useTheme } from "../ThemeContext";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Contact from "../pages/contact";
+
 
 export default function Nav() {
   const { theme, toggleTheme } = useTheme();
@@ -9,52 +10,54 @@ export default function Nav() {
   const navbarTheme = theme === "light" ? "navbar-light bg-light" : "navbar-dark bg-dark";
   const inverseTheme = theme === "light" ? "dark" : "light";
   return (
-    <BrowserRouter>
-      <header className="sticky-top">
-        <nav className={`navbar navbar-expand-sm ${navbarTheme}`}>
-          <div className="container-fluid">
-            <a className="navbar-brand d-flex justify-content-center align-items-center" href="" height="50">
-              <img className="rounded-pill" src={appLogo} alt="Logo" width="45" height="45" />
-              &nbsp;
-              <span>PROP80</span>
-            </a>
+    <header className="sticky-top">
+      <nav className={`navbar navbar-expand-sm ${navbarTheme}`}>
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex justify-content-center align-items-center" href="" height="50">
+            <img className="rounded-pill" src={appLogo} alt="Logo" width="45" height="45" />
+            &nbsp;
+            <span>PROP80</span>
+          </a>
 
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <a className="nav-link" to="/pages/contact">
-                  Contact&nbsp;Us
-                </a>
-              </li>
-            </ul>
-            <button data-bs-toggle="offcanvas" data-bs-target="#favourites" className={`favouriteBtn mx-1 p-1 border-2 d-flex justify-content-center align-items-center rounded-pill btn btn-outline-danger`} title="View Your Favourites">
-              <span className="label">&nbsp;&nbsp;My&nbsp;Favourites&nbsp;&nbsp;</span>
-              <span className="m-0 material-symbols-rounded bg-danger text-white rounded-pill d-flex justify-content-center align-items-center">
-                favorite
-              </span>
-              
-            </button>
-            <button onClick={toggleTheme} className={`mx-1 themeToggle d-flex justify-content-center align-items-center rounded-pill btn btn-${inverseTheme}`} title={theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}>
-              <span className="material-symbols-rounded">
-                {theme === "light" ? "dark_mode" : "light_mode"}
-              </span>
-            </button>
-          </div>
-        </nav>
-        <div className={`offcanvas offcanvas-end bg-${theme} text-${inverseTheme}`} id="favourites">
-          <div className="offcanvas-header d-flex justify-content-between align-items-center">
-            <h4 className="offcanvas-title">My Favourites</h4>
-            <button data-bs-dismiss="offcanvas" className={`mx-1 closeFavourites d-flex justify-content-center align-items-center rounded-pill btn btn-${inverseTheme}`} title="Close Favourites"><span className="material-symbols-rounded">close</span></button>
-          </div>
-          <div className="offcanvas-body">
-
-          </div>
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <button className="nav-link" onClick={<Contact/>}></button>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" to="/pages/contact">
+                
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Another Link
+              </a>
+            </li>
+          </ul>
+          <button data-bs-toggle="offcanvas" data-bs-target="#favourites" className={`favouriteBtn mx-1 p-1 border-2 d-flex justify-content-center align-items-center rounded-pill btn btn-outline-danger`} title="View Your Favourites">
+            <span className="label">&nbsp;&nbsp;My&nbsp;Favourites&nbsp;&nbsp;</span>
+            <span className="m-0 material-symbols-rounded bg-danger text-white rounded-pill d-flex justify-content-center align-items-center">
+              favorite
+            </span>
+            
+          </button>
+          <button onClick={toggleTheme} className={`mx-1 themeToggle d-flex justify-content-center align-items-center rounded-pill btn btn-${inverseTheme}`} title={theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}>
+            <span className="material-symbols-rounded">
+              {theme === "light" ? "dark_mode" : "light_mode"}
+            </span>
+          </button>
         </div>
-      </header>
-      <Routes>
-        <Route path="/pages/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+      </nav>
+      <div className={`offcanvas offcanvas-end bg-${theme} text-${inverseTheme}`} id="favourites">
+        <div className="offcanvas-header d-flex justify-content-between align-items-center">
+          <h4 className="offcanvas-title">My Favourites</h4>
+          <button data-bs-dismiss="offcanvas" className={`mx-1 closeFavourites d-flex justify-content-center align-items-center rounded-pill btn btn-${inverseTheme}`} title="Close Favourites"><span className="material-symbols-rounded">close</span></button>
+        </div>
+        <div className="offcanvas-body">
 
+        </div>
+      </div>
+    </header>
     
   );
 }
