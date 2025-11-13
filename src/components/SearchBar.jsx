84d345f0b56ab6792a23c1903ad1a7a5984f6dbb
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-13 15:30:38 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,8 @@ import { useTheme } from "../ThemeContext";
 export default function SearchBar() {
 
     return(
-        <div className="container-fluid p-5 bg-primary text-white text-center pt-5">
+        <div className="searchBarSection container-fluid p-5 text-white text-center">
+            <div className="space"></div>
             <h1>My First Bootstrap Page</h1>
             <p>Resize this responsive page to see the effect!</p> 
         </div>
