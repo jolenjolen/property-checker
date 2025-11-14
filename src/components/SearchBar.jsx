2ed021e4abc9117952278515048d1c91244acf6e
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-14 13:40:05 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
                 <h2>Find The Perfect Destination</h2>
                 <p>Explore the vibrant beaches and embrace the serene countryside</p>
                 <form className="search-form rounded-pill">
-                    <div className="input-group d-flex justify-content-center align-items-center">
+                    <div className="input-group">
                         <span className="m-1 input-group-text material-symbols-rounded">beach_access</span>
                         <input type="text" className=" m-1 form-control" placeholder="Whats on your mind?" />
                         <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
