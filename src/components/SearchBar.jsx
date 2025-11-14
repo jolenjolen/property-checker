a8d4018f29b98a9e0798190ec0b927c1e869a552
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-14 13:15:53 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,10 +5,22 @@ export default function SearchBar() {
     const { theme, toggleTheme } = useTheme();
 
     return(
-        <div className={`searchBarSection-${theme} container-fluid p-5 text-center`}>
+        <>
             <div className="space"></div>
-            <h1>My First Bootstrap Page</h1>
-            <p>Resize this responsive page to see the effect!</p> 
-        </div>
+            <div className="hero rounded-4 m-2 d-flex flex-column justify-content-center align-items-center">
+                <h2>Find The Perfect Destination</h2>
+                <p>Explore the vibrant beaches and embrace the serene countryside</p>
+                <form className="search-form rounded-pill">
+                    <div classname="input-group">
+                        <span classname="m-1 input-group-text material-symbols-rounded">beach_access</span>
+                        <input type="text" className=" m-1 form-control" placeholder="Whats on your mind?" />
+                        <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
+                            <span className="material-symbols-rounded">search</span>
+                        </button>
+                        <a className="open-map rounded-pill m-1 d-flex justify-content-center align-items-center" href="http://"><span className="material-symbols-rounded">map</span></a>
+                    </div>
+                </form>
+            </div>
+        </>
     );
 }
