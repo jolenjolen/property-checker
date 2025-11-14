cb18aa3b9b462355daddb57ab1f6e2e9c1fb5752
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-14 13:30:39 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,15 @@ import { useTheme } from "../ThemeContext";
 
 export default function SearchBar() {
     const { theme, toggleTheme } = useTheme();
-
+    const inverseTheme = theme === "light" ? "dark" : "light";
     return(
         <>
             <div className="space"></div>
-            <div className={`hero hero-${theme} rounded-4 m-2 d-flex flex-column justify-content-center align-items-center`}>
+            <div className={`hero hero-${theme} rounded-4 m-2 d-flex flex-column justify-content-center align-items-center text-${inverseTheme}`}>
                 <h2>Find The Perfect Destination</h2>
                 <p>Explore the vibrant beaches and embrace the serene countryside</p>
                 <form className="search-form rounded-pill">
-                    <div classname="input-group">
+                    <div classname="input-group d-flex justify-content-center align-items-center">
                         <span classname="m-1 input-group-text material-symbols-rounded">beach_access</span>
                         <input type="text" className=" m-1 form-control" placeholder="Whats on your mind?" />
                         <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
