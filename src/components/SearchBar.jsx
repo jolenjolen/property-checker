ab46909334a6eaacd6d0ce810045fcd199e2299a
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-11-20 14:20:46 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,14 +16,16 @@ export default function SearchBar() {
                 <h1 className={`hero-text-heading text-${inverseTheme}`}><span className="heroFirstText">Find</span> Your Next Home</h1>
                 <p className={`hero-text-slogan text-${inverseTheme}`}>Believe in finding it with the UK's largest choice of homes</p>
                 <form className={`hero-searchbar-group search-form shadow-sm ${isRounded ? "rounded-pill" : "rounded"}`}>
-                    <div className="input-group">
-                        <span className="m-1 input-group-text material-symbols-rounded">cottage</span>
-                        <input type="text" className=" m-1 form-control" placeholder="e.g. Bath, UB3, or Leeds" />
-                        <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
-                            <span className="material-symbols-rounded">search</span>
-                        </button>
-                        <a className="open-map rounded-pill m-1 d-flex justify-content-center align-items-center" href="http://"><span className="material-symbols-rounded">map</span></a>
-                        <button onClick={toggleRounded} type="button" className="m-1 rounded-pill d-flex justify-content-center align-items-center expand-search-bar" data-bs-toggle="collapse" data-bs-target="#expand-search-bar"><span className="material-symbols-rounded">keyboard_arrow_down</span></button>
+                    <div className="display-flex flex-column justify-content-center">
+                        <div className="input-group">
+                            <span className="m-1 input-group-text material-symbols-rounded">cottage</span>
+                            <input type="text" className=" m-1 form-control" placeholder="e.g. Bath, UB3, or Leeds" />
+                            <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
+                                <span className="material-symbols-rounded">search</span>
+                            </button>
+                            <a className="open-map rounded-pill m-1 d-flex justify-content-center align-items-center" href="http://"><span className="material-symbols-rounded">map</span></a>
+                            <button onClick={toggleRounded} type="button" className="m-1 rounded-pill d-flex justify-content-center align-items-center expand-search-bar" data-bs-toggle="collapse" data-bs-target="#expand-search-bar"><span className="material-symbols-rounded">keyboard_arrow_down</span></button>
+                        </div>
                         <div id="expand-search-bar" class="collapse">
                             <button>Search radius</button>
                             <button>Property types</button>
