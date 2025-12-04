caa5c1febd6a0db7d42a4add308286d2936fc6e8
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-04 11:58:52 +0000

    push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,29 +12,61 @@ export default function SearchBar() {
     };
     return(
         <>
-            <div className={`hero hero-${theme} rounded-4 m-2 d-flex flex-column justify-content-center align-items-center`}>
-                <h1 className={`hero-text-heading text-${inverseTheme}`}><span className="heroFirstText">Find</span> Your Next Home</h1>
-                <p className={`hero-text-slogan text-${inverseTheme}`}>Believe in finding it with the UK's largest choice of homes</p>
-                <form className={`sticky-top hero-searchbar-group search-form shadow-sm ${isRounded ? "rounded-pill" : "rounded"}`}>
-                    <div className="display-flex flex-column justify-content-center">
-                        <div className="input-group">
-                            <span className="m-1 input-group-text material-symbols-rounded">cottage</span>
-                            <input type="text" className=" m-1 form-control" placeholder="e.g. Bath, UB3, or Leeds" />
-                            <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
-                                <span className="material-symbols-rounded">search</span>
-                            </button>
-                            <a className="open-map rounded-pill m-1 d-flex justify-content-center align-items-center" href="http://"><span className="material-symbols-rounded">map</span></a>
-                            <button onClick={toggleRounded} type="button" className="m-1 rounded-pill d-flex justify-content-center align-items-center expand-search-bar" data-bs-toggle="collapse" data-bs-target="#expand-search-bar"><span className="material-symbols-rounded">{isRounded ? "keyboard_arrow_down" : "keyboard_arrow_up"}</span></button>
-                        </div>
-                        <div id="expand-search-bar" className="collapse">
-                            <button>Search radius</button>
-                            <button>Property types</button>
-                            <button>No. of bedrooms</button>
-                        </div>
-                    </div>
-                </form>
+      <div className={`hero hero-${theme} rounded-4 m-2 d-flex flex-column justify-content-center align-items-center`}>
+        <h1 className={`hero-text-heading text-${inverseTheme}`}>
+          <span className="heroFirstText">Find</span> Your Next Home
+        </h1>
+
+        <p className={`hero-text-slogan text-${inverseTheme}`}>
+          Believe in finding it with the UK's largest choice of homes
+        </p>
+
+        <form className={`sticky-top hero-searchbar-group search-form shadow-sm ${isRounded ? "rounded-pill" : "rounded"}`}>
+          <div className="d-flex flex-column justify-content-center">
+
+            <div className="input-group">
+              <span className="m-1 input-group-text material-symbols-rounded">cottage</span>
+
+              <input
+                type="text"
+                className="m-1 form-control"
+                placeholder="e.g. Bath, UB3, or Leeds"
+              />
+
+              <button type="submit" className="m-1 rounded-pill btn search-btn d-flex justify-content-center align-items-center">
+                <span className="material-symbols-rounded">search</span>
+              </button>
+
+              <a className="open-map rounded-pill m-1 d-flex justify-content-center align-items-center">
+                <span className="material-symbols-rounded">map</span>
+              </a>
+
+              <button
+                onClick={toggleRounded}
+                type="button"
+                className="m-1 rounded-pill d-flex justify-content-center align-items-center expand-search-bar"
+                data-bs-toggle="collapse"
+                data-bs-target="#expand-search-bar"
+              >
+                <span className="material-symbols-rounded">
+                  {isRounded ? "keyboard_arrow_down" : "keyboard_arrow_up"}
+                </span>
+              </button>
             </div>
-            <h1 className="lol">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates excepturi hic, labore quidem consectetur perspiciatis impedit maxime. Velit, nesciunt esse libero aliquam est, dolor voluptatem minima possimus dignissimos eaque quisquam.</h1>
-        </>
+
+            <div id="expand-search-bar" className="collapse extra-search-options">
+              <button>Search radius</button>
+              <button>Property types</button>
+              <button>No. of bedrooms</button>
+            </div>
+
+          </div>
+        </form>
+      </div>
+
+      <h1 className="lol">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit…
+      </h1>
+    </>
     );
 }
