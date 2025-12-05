8c47e8394c0e23cb2443fb7389a8ba2030b76042
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-05 11:24:38 +0000

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from './components/Nav'
+import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./ThemeContext";
-import './App.css'
-import SearchBar from './components/SearchBar';
-import Cards from "./components/Cards"
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+
 export default function App() {
-
   return (
-    <>
-      <ThemeProvider>
-        <BrowserRouter>
-          <Nav />
-
-          <Routes>
-            <Route path="/" element={<Cards />} />
-            <Route path="/search" element={<SearchBar />} />
-            {/* Add more pages: */}
-            <Route path="/buy" element={<h1>Buy Page</h1>} />
-            <Route path="/rent" element={<h1>Rent Page</h1>} />
-            <Route path="/agent" element={<h1>Agent Page</h1>} />
-            <Route path="/contact" element={<h1>Contact Page</h1>} />
-          </Routes>
-
-        </BrowserRouter>
-      </ThemeProvider>
-    </>
-  )
+    <ThemeProvider>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "../components/SearchBar";
+import Cards from "../components/Cards";
+
+export default function Home() {
+  return (
+    <>
+      <SearchBar />
+      <Cards />
+    </>
+  );
+}
