0cf732c7522c22c837bfb13d1a6b6c15f4a61760
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-05 18:41:01 +0000

    push

--- src/ReactBits/GlareHover.jsx
+++ src/ReactBits/GlareHover.jsx
@@ -2,7 +2,7 @@ import "./GlareHover.css";
 import { useTheme } from "../ThemeContext";
 
 const GlareHover = ({
-  width = "800px",
+  width = "300px",
   height = "500px",
   background,
   borderRadius = "15px",
