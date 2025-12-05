83dfa2f5df0cbeca4906e52f8bba62a4dfe4fc40
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-05 11:58:22 +0000

    push

--- src/ReactBits/ClickSpark.jsx
+++ src/ReactBits/ClickSpark.jsx
@@ -136,7 +136,8 @@ const ClickSpark = ({
       style={{
         position: 'relative',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        zIndex: 9999
       }}
       onClick={handleClick}
     >
