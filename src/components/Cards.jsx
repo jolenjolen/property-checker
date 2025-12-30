938521245e4d5b5efb0b72938e69d5f80be61645
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-30 19:38:13 +0000

    push

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,14 +10,22 @@ export default function Cards({ properties }) {
       <div className="row g-4">
         {properties.map((property) => (
           <div className="col-md-4" key={property.id}>
-            <div className="card h-100 shadow-sm">
-              <img
-                src={property.picture}
-                className="card-img-top"
-                alt={property.type}
-              />
-
-              <div className="card-body">
+            <div style={{ height: '600px', position: 'relative' }} className="m-2 d-flex justify-content-center align-items-center">
+                <GlareHover
+                    
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={300}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                </GlareHover>
+                <img
+                    src={property.picture}
+                    className="card-img-top"
+                    alt={property.type}
+                />
+                <div className="card-body">
                 <h5 className="card-title">
                   £{property.price.toLocaleString()}
                 </h5>
@@ -40,4 +48,28 @@ export default function Cards({ properties }) {
       </div>
     </div>
   );
+}
+
+export function CardsX(){
+    const { theme } = useTheme();
+    const inverseTheme = theme === "light" ? "dark" : "light";
+
+    return(
+        <>
+            <div style={{ height: '600px', position: 'relative' }} className="m-2 d-flex justify-content-center align-items-center">
+                <GlareHover
+                    
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={300}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                </GlareHover>
+            </div>
+
+
+        </>
+    )
+
 }
