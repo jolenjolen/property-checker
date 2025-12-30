4c3f7bf3e0547ad8524ec051fc591da025cf624f
Jolen Mark Mascarenhas <69632378+jolenjolen@users.noreply.github.com>
2025-12-30 20:53:11 +0000

    push

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { FavouritesProvider } from "./FavouritesContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
